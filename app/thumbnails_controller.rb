e7e8477b220cb04c18c84252efafb885ee2f8856
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-06-25 18:55:39 +0900

    add close button

--- app/thumbnails_controller.rb
+++ app/thumbnails_controller.rb
@@ -3,13 +3,24 @@ class ThumbnailsController < UIViewController
 
   def loadView
     if super
-      view.backgroundColor = UIColor.whiteColor
+      view.backgroundColor = UIColor.darkGrayColor
+      close_button = UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
+        b.setTitle('閉じる', forState:UIControlStateNormal)
+        b.frame = [[10, 374], [60, 30]]
+        b.alpha = 0.8
+        b.addTarget(self, action:'close', forControlEvents:UIControlEventTouchUpInside)
+      end
+      view.addSubview(close_button)
     end
     self
   end
 
   def viewDidLoad
     p @url
+  end
+
+  def close
+    self.dismissModalViewControllerAnimated(true)
   end
 
 end

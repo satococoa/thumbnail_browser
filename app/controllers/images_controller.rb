a317c00c7e166f5b888bb98d2dbdd9508cf0623b
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-06-27 16:56:03 +0900

    display images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,52 +2,31 @@ class ImagesController < UIViewController
   # 画像のURL(NSURL)の入った配列
   attr_accessor :images
 
-  LOADING_IMAGE = UIImage.imageNamed('loading.png')
-
   def loadView
     if super
       view.backgroundColor = UIColor.darkGrayColor
 
-      # 画像を取得して表示するキュー
-      @queue = NSOperationQueue.new
-
-      @image_view = UIImageView.alloc.initWithFrame([[10, 10], [300, 396]]).tap do |img|
-        img.backgroundColor = UIColor.whiteColor
-      end
-      view.addSubview(@image_view)
-
-      @thumbnail_view = UIScrollView.alloc.initWithFrame([[0, 416], [320, 44]]).tap do |sc|
-        sc.pagingEnabled = true
-        sc.alpha = 0.8
-        sc.backgroundColor = UIColor.blackColor
-      end
-      view.addSubview(@thumbnail_view)
+      @images_view = ImagesView.alloc.initWithFrame(content_frame)
+      view.addSubview(@images_view)
       
       # 閉じるボタン
-      close_button = UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
-        b.setTitle('閉じる', forState:UIControlStateNormal)
-        b.frame = [[20, 374], [60, 30]]
-        b.alpha = 0.8
-        b.addTarget(self, action:'close', forControlEvents:UIControlEventTouchUpInside)
+      @images_view.close_button.when(UIControlEventTouchUpInside) do
+        self.dismissModalViewControllerAnimated(true)
       end
-      view.addSubview(close_button)
     end
     self
   end
 
   def viewWillAppear(animated)
+    @images_view.images = @images
+    @images_view.setNeedsDisplay
     # とりあえず表示
     # TODO: あとでviewでちゃんとやる
-    req = NSURLRequest.requestWithURL(@images.last)
-    @image_view.setImageWithURLRequest(req,
-      placeholderImage:LOADING_IMAGE,
-      success:lambda {|req, res, img| p img },
-      failure:lambda {|req, res, error| log_error error }
-    )
-  end
-
-  def close
-    @queue.cancelAllOperations
-    self.dismissModalViewControllerAnimated(true)
+    # req = NSURLRequest.requestWithURL(@images.last)
+    # @image_view.setImageWithURLRequest(req,
+    #   placeholderImage:LOADING_IMAGE,
+    #   success:lambda {|req, res, img| p img },
+    #   failure:lambda {|req, res, error| log_error error }
+    # )
   end
 end
--- app/views/images_view.rb
+++ app/views/images_view.rb
@@ -1,11 +1,63 @@
 class ImagesView < UIView
   attr_accessor :images
-  attr_reader :stage
+  attr_reader :stage, :thumbnails, :close_button
+
+  LOADING_IMAGE = UIImage.imageNamed('loading.png')
 
   def initWithFrame(rect)
     if super
-
+      @images = []
+      self.backgroundColor = UIColor.darkGrayColor
+      @stage = UIScrollView.alloc.initWithFrame([[0, 0], [320, 411]]).tap do |v|
+        v.pagingEnabled = true
+        # v.contentInset = [10, 10, 10, 10]
+      end
+      @thumbnails = UIScrollView.alloc.initWithFrame([[0, 411], [320, 49]]).tap do |v|
+        v.pagingEnabled = true
+        v.backgroundColor = UIColor.blackColor
+        # v.contentInset = [4, 50, 5, 50]
+      end
+      @close_button = UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
+        b.setTitle('閉じる', forState:UIControlStateNormal)
+        b.frame = [[20, 374], [60, 30]]
+        b.alpha = 0.8
+      end
+      
+      addSubview(@stage)
+      addSubview(@thumbnails)
+      addSubview(@close_button)
     end
     self
   end
+
+  def drawRect(rect)
+    @stage.contentSize = [320*@images.count, 411-20] # 20はinset分
+    @thumbnails.contentSize = [320*(1 + @images.count/4), 40]
+    @images.each_with_index do |img, i|
+      req = NSURLRequest.requestWithURL(img)
+
+      stage_offset = i * 320 + 10
+      stage_image = UIImageView.alloc.initWithFrame([[stage_offset, 10], [300, 411-20]]).tap do |stg|
+        stg.contentMode = UIViewContentModeScaleAspectFit
+        stg.setImageWithURLRequest(req,
+          placeholderImage:LOADING_IMAGE,
+          success:lambda {|req, res, img| p img },
+          failure:lambda {|req, res, error| log_error error }
+        )
+      end
+      @stage.addSubview(stage_image)
+
+      thumb_offset = i/4 * 320 + i%4 * 60 + 50
+      thumb_image = UIImageView.alloc.initWithFrame([[thumb_offset, 5], [40, 40]]).tap do |thumb|
+        thumb.contentMode = UIViewContentModeScaleAspectFit
+        thumb.setImageWithURLRequest(req,
+          placeholderImage:LOADING_IMAGE,
+          success:lambda {|req, res, img| p img },
+          failure:lambda {|req, res, error| log_error error }
+        )
+      end
+      @thumbnails.addSubview(thumb_image)
+    end
+  end
+
 end
